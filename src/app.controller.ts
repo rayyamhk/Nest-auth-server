1942dca9be56fef28b5dd04ae863f0fb813a2249
rayyamhk <rayyamhk@gmail.com>
2022-12-08 01:15:39 -0500

    add rate-limiter

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get('_health')
-  healthCheck() {
-    return 'OK';
-  }
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './modules/auth/auth.module';
 import { UtilsModule } from './modules/utils/utils.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UtilsModule, AuthModule],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 300,
+      limit: 10,
+    }),
+    UtilsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
